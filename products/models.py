abe9415f3ace6b3afcf4931aeeae8e12b54bb28e
Vyas Cholayil <vyascholayil@gmail.com>
2019-12-13 20:32:19 -0500

    Added model attributes

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.TextField()
+    pub_date = models.DateField()
+    votes_total = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='images/')
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
