060819605df3e157b352261677e45962245445f7
Vyas Cholayil <vyascholayil@gmail.com>
2019-12-14 14:46:43 -0500

    Added model attributes and created functions for adding products

--- products/models.py
+++ products/models.py
@@ -1,17 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=255)
     url = models.TextField()
-    pub_date = models.DateField()
-    votes_total = models.PositiveIntegerField()
+    date = models.DateField()
+    votes_total = models.IntegerField(default=1)
+    icon = models.ImageField(upload_to='images/')
     image = models.ImageField(upload_to='images/')
     body = models.TextField()
-
-    def __str__(self):
-        return self.title
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def summary(self):
         return self.body[:100]
+
+    def __str__(self):
+        return self.title
--- products/views.py
+++ products/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Product
+import datetime
 
 
 # Create your views here.
 def home(request):
     return render(request, 'products/home.html')
+
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and \
+                request.FILES['image']:
+            product = Product()
+            product.title = request.POST['title']
+            product.body = request.POST['body']
+            if request.POST.get('url').startswith('http://') or request.POST.get('url').startswith('https://'):
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://' + request.POST['url']
+            product.icon = request.FILES['icon']
+            product.image = request.FILES['image']
+            product.date = datetime.datetime.now().date()
+            product.hunter = request.user
+            product.save()
+            return redirect('home')
+        else:
+            return render(request, 'products/create.html', {'error': 'All Fields are required'})
+    else:
+        return render(request, 'products/create.html')
