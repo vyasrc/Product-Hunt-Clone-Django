2e0a7187ecbb2bd648b5d3806f8db54cd56c316c
Vyas Cholayil <vyascholayil@gmail.com>
2019-12-15 14:57:27 -0500

    Added details functionality for products and designed the home page.

--- products/models.py
+++ products/models.py
@@ -14,7 +14,12 @@ class Product(models.Model):
     hunter = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def summary(self):
-        return self.body[:100]
+        return self.body[:145]
 
     def __str__(self):
         return self.title
+
+
+class Vote(models.Model):
+    productID = models.ForeignKey(Product,on_delete=models.CASCADE)
+    userID = models.ForeignKey(User,on_delete=models.CASCADE)
--- products/views.py
+++ products/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Product
+from .models import Product, Vote
 import datetime
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'products/home.html')
+    products = Product.objects
+    return render(request, 'products/home.html', {'products': products})
 
 
-@login_required
+@login_required(login_url='/accounts/login')
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and \
@@ -26,8 +27,33 @@ def create(request):
             product.date = datetime.datetime.now().date()
             product.hunter = request.user
             product.save()
-            return redirect('home')
+            return redirect('/products/' + str(product.id))
         else:
             return render(request, 'products/create.html', {'error': 'All Fields are required'})
     else:
         return render(request, 'products/create.html')
+
+
+def detail(request, product_id):
+    detail_product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/detail.html', {'product': detail_product})
+
+
+@login_required(login_url='/accounts/login')
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        try:
+            vote = Vote.objects.get(productID=product_id, userID=request.user)
+        except Vote.DoesNotExist:
+            vote = None
+
+        if vote is None and product.hunter != request.user:
+            # find product by id and increment
+            vote = Vote(productID=product, userID=request.user)
+            product.votes_total += 1
+            vote.save()
+            product.save()
+        return redirect('/products/' + str(product.id))
+
+
